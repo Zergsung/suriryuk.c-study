24b69fd9817311fec1180d502a85455a197a6974
Zergsung <usungmo0317@gmail.com>
2022-11-07 15:48:04 +0900

    함수사용으로 변경..

--- Fibonacci/code.c
+++ Fibonacci/code.c
@@ -1,14 +1,18 @@
 #include <stdio.h>
 
-int main() {
-  int num1=0, num2=1, nextnum, i, output, input;
-
-  scanf("%d", &input);
-  
-  for(i = 0; i <= input; i++) {
+int fibonacci(int a) {
+  int  num1=0, num2=1, nextnum, i, input;
+  for(i = 0; i <= a; i++) {
     printf("%d ", num1);
     nextnum = num1 + num2;
     num2 = num1;  
     num1 = nextnum;
   }
+}
+
+int main() {
+  int input;
+
+  scanf("%d", &input);
+  fibonacci(input);
 }
